d06436bfe0e5c95efb79a4ba9e6fab5e5c836a32
kanpalRavi <kanpalravindra21@gmail.com>
2024-03-12 22:01:22 +0530

    lerning import and export in javascript for not writing a same code again and again

---
+++ import_Export/components/navbar.js
@@ -0,0 +1,22 @@
+//export :- giving
+//import :- taking
+
+let navbar = () =>{
+    return ` <h3>
+        <a href="index.html">home</a>
+        </h3>
+        <h3>
+            <a href="jewellery.html">Jewellery</a>
+        </h3>
+        <h3>
+            <a href="electroincs.html">electroincs</a>
+        </h3>
+        <h3>
+            <a href="">login</a>
+        </h3>
+        <h3>
+            <a href="">signup</a>
+        </h3>`;
+}
+
+export default navbar; //default export ***
